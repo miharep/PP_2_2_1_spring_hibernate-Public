a9e73d4dcd34708d5556cb5d20edf1c650980498
Михаил <mih.borey@gmail.com>
2024-02-08 21:46:45 +0500

    add Car class

--- 2.2.1. spring_hibernate/src/main/java/hiber/config/AppConfig.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/config/AppConfig.java
@@ -1,5 +1,6 @@
 package hiber.config;
 
+import hiber.model.Car;
 import hiber.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -43,9 +44,14 @@ public class AppConfig {
       Properties props=new Properties();
       props.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
       props.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
-
+      
+      props.put("hibernate.format_sql", env.getProperty("hibernate.format_sql"));
+      props.put("hibernate.highlight_sql", env.getProperty("hibernate.highlight_sql"));
+      props.put("hibernate.use_nationalized_character_data", env.getProperty("hibernate.use_nationalized_character_data"));
+      
       factoryBean.setHibernateProperties(props);
-      factoryBean.setAnnotatedClasses(User.class);
+      factoryBean.setAnnotatedClasses(User.class, Car.class);
+      
       return factoryBean;
    }
 
--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -18,13 +18,18 @@ public class User {
 
    @Column(name = "email")
    private String email;
+   
+   @OneToOne(cascade = CascadeType.ALL)
+   @JoinColumn(name = "car_id")
+   private Car car;
 
    public User() {}
    
-   public User(String firstName, String lastName, String email) {
+   public User(String firstName, String lastName, String email, Car car) {
       this.firstName = firstName;
       this.lastName = lastName;
       this.email = email;
+      this.car = car;
    }
 
    public Long getId() {
@@ -57,5 +62,24 @@ public class User {
 
    public void setEmail(String email) {
       this.email = email;
+   }
+   
+   public Car getCar() {
+      return car;
+   }
+
+   public void setCar(Car car) {
+      this.car = car;
+   }
+   
+   @Override
+   public String toString() {
+      return "User {" +
+              "Id = " + id +
+              ", First Name = '" + firstName + '\'' +
+              ", LastName = '" + lastName + '\'' +
+              ", Email = '" + email + '\'' +
+              ", Car = " + car.toString() +
+              " }";
    }
 }
